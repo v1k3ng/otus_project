564815cb02b4bfab8bed51224816e991d71442bf
Demenev Maksim <6254918+v1k3ng@users.noreply.github.com>
2019-07-01 11:17:58 +0500

    Test tests crawler-bot

--- test/crawler-bot-test.sh
+++ test/crawler-bot-test.sh
@@ -7,14 +7,17 @@ docker run -d --rm --name bot-test \
     --cap-add=NET_ADMIN \
     --privileged \
     --device /dev/net/tun \
-    mad72/bot-tests:latest ash
-docker images
-sleep 30
-docker container ls -a
-docker exec bot-test \
+    mad72/bot-tests:latest \
     "cd /app \
     && pip3 install -r requirements-test.txt \
     && python3 -m unittest discover -s tests/"
+# docker images
+# sleep 30
+# docker container ls -a
+# docker exec bot-test \
+#     "cd /app \
+#     && pip3 install -r requirements-test.txt \
+#     && python3 -m unittest discover -s tests/"
 
 
 # GROUP=2019-02
