1f08ec3681c079fb45713ef3161faa3d129f25d5
Demenev Maksim <6254918+v1k3ng@users.noreply.github.com>
2019-07-01 17:21:15 +0500

    Added terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,11 +30,6 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   node_config {
     preemptible  = true
     machine_type = "${var.machine_type}"
-
-    // metadata {
-    //   disable-legacy-endpoints = "true"
-    // }
-
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
