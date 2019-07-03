440f2c84267dc30103381014a4cd1fd7c5413105
Demenev Maksim <6254918+v1k3ng@users.noreply.github.com>
2019-07-03 15:52:04 +0500

    Test multi-env

--- deploy_app/deploy-app.sh
+++ deploy_app/deploy-app.sh
@@ -10,9 +10,39 @@
 # TRAVIS_PULL_REQUEST_BRANCH:
 #     if the current job is a pull request, the name of the branch from which the PR originated.
 #     if the current job is a push build, this variable is empty ("").
-# TRAVIS_PULL_REQUEST_SHA:
-#     if the current job is a pull request, the commit SHA of the HEAD commit of the PR.
-#     if the current job is a push build, this variable is empty ("").
-# TRAVIS_PULL_REQUEST_SLUG:
-#     if the current job is a pull request, the slug (in the form owner_name/repo_name) of the repository from which the PR originated.
-#     if the current job is a push build, this variable is empty ("").
+
+PROD=prod
+
+cd deploy_app/
+case "$TRAVIS_EVENT_TYPE" in
+push)
+    if [ "$TRAVIS_BRANCH" -eq "master" ]
+    then
+        # kubectl apply - $PROD -f deployment-mongodb.yml -f deployment-rabbitmq.yml -f service-mongodb.yml -f service-rabbitmq.yml
+        # sleep 30
+        kubectl delete deployments crawler-bot
+        kubectl delete deployments crawler-ui
+        kubectl apply -n $PROD -f deployment-bot.yml -f deployment-ui.yml -f service-bot.yml -f service-ui.yml
+        sleep 60
+        kubectl get svc -n $PROD
+    else
+        kubectl create namespace $TRAVIS_BRANCH
+        kubectl apply -n $TRAVIS_BRANCH -f deployment-mongodb.yml -f deployment-rabbitmq.yml -f service-mongodb.yml -f service-rabbitmq.yml
+        sleep 30
+        kubectl delete -n $TRAVIS_BRANCH deployments crawler-bot
+        kubectl delete -n $TRAVIS_BRANCH deployments crawler-ui
+        kubectl apply -n $TRAVIS_BRANCH -f deployment-bot.yml -f deployment-ui.yml -f service-bot.yml -f service-ui.yml
+        sleep 60
+        kubectl get svc -n $TRAVIS_BRANCH
+    fi
+;;
+pull_request)
+    kubectl delete all -n $TRAVIS_PULL_REQUEST_BRANCH
+    kubectl delete namespace $TRAVIS_PULL_REQUEST_BRANCH
+    kubectl delete -n $PROD deployments crawler-bot
+    kubectl delete -n $PROD deployments crawler-ui
+    kubectl apply -n $PROD -f deployment-bot.yml -f deployment-ui.yml -f service-bot.yml -f service-ui.yml
+    sleep 60
+    kubectl get svc -n $PROD
+;;
+esac
