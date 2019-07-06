1eb30c846f2edbbc2d70493e00250941ad32397b
Demenev Maksim <6254918+v1k3ng@users.noreply.github.com>
2019-07-06 22:20:17 +0500

    Added charts for prometheus and grafana. Alertmanager is worked.

--- cluster.sh
+++ cluster.sh
@@ -48,8 +48,8 @@ deploy_namespaces()
 {
     echo -e "\n${CYAN}Deploing PROD namespace${NONE}\n-----------------------"
     cd $SCRIPT_PATH
-    kubectl apply -f deploy_app/namespace-prod.yml
-    kubectl apply -f k8s/namespace-monitoring.yml
+    kubectl apply -f deploy_app/namespace-prod.yml --wait
+    kubectl apply -f k8s/namespace-monitoring.yml --wait
 }
 
 deploy_rabbit_mongo()
@@ -63,16 +63,29 @@ deploy_rabbit_mongo()
         -f deploy_app/deployment-mongodb-exporter.yml \
         -f deploy_app/service-mongodb-exporter.yml \
         -f deploy_app/deployment-rabbitmq-exporter.yml \
-        -f deploy_app/service-rabbitmq-exporter.yml
+        -f deploy_app/service-rabbitmq-exporter.yml \
+        --wait
 
+}
+
+deploy_base_app()
+{
+    echo -e "\n${CYAN}Deploing BOT and UI${NONE}\n-----------------------"
+    cd $SCRIPT_PATH
+    kubectl apply -n prod \
+        -f deploy_app/deployment-bot.yml \
+        -f deploy_app/deployment-ui.yml \
+        -f deploy_app/service-bot.yml \
+        -f deploy_app/service-ui.yml \
+        --wait
 }
 
 deploy_helm()
 {
     echo -e "\n${CYAN}Deploing HELM${NONE}\n-------------"
     cd $SCRIPT_PATH
-    kubectl apply -f k8s/tiller.yml
-    helm init --service-account tiller
+    kubectl apply -f k8s/tiller.yml --wait
+    helm init --service-account tiller --wait
     helm list --all
 }
 
@@ -80,11 +93,14 @@ deploy_prometheus()
 {
     echo -e "\n${CYAN}Deploing PROMETHEUS${NONE}\n-------------"
     cd $SCRIPT_PATH
-    kubectl apply \
-        -f k8s/prometheus-rbac.yml \
-        -f k8s/prometheus-config-map.yml \
-        -f k8s/prometheus-deployment.yml \
-        -f k8s/prometheus-service.yml 
+    # kubectl apply \
+    #     -f k8s/prometheus-rbac.yml \
+    #     -f k8s/prometheus-config-map.yml \
+    #     -f k8s/prometheus-deployment.yml \
+    #     -f k8s/prometheus-service.yml
+    helm upgrade --namespace monitoring prometheus charts/prometheus \
+            -f charts/prometheus/values.yaml \
+            --install --wait
 }
 
 deploy_grafana()
@@ -96,55 +112,58 @@ deploy_grafana()
     #     -f k8s/grafana-daemonset-nodeexporter.yml \
     #     -f k8s/grafana-deployment-state-metrics.yml \
     #     -f k8s/grafana-rbac-state-metrics.yml 
-    # wait for tiller
-    sleep 30
-    helm upgrade --namespace="monitoring" grafana grafana/grafana -f grafana/grafana/values.yaml --install --set "adminPassword=admin"
-    # helm upgrade --namespace="monitoring" --install grafana stable/grafana --set "adminPassword=admin"
+    helm upgrade --namespace monitoring grafana charts/grafana/grafana \
+        -f charts/grafana/grafana/values.yaml \
+        --install \
+        --set "adminPassword=admin" \
+        --wait
 }
 
 output_values()
 {
+    echo -e "\n${CYAN}Output values${NONE}\n-------------"
+    kubectl get svc -n prod
     POD_GRAFANA=`kubectl get pods -n monitoring -l "app=grafana" -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}'`
-    POD_PROMETHEUS=`kubectl get pods -n monitoring -l "app=prometheus-server" -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}'`
+    POD_PROMETHEUS=`kubectl get pods -n monitoring -l "app=prometheus,component=alertmanager" -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}'`
     echo -e "\n\nkubectl --namespace monitoring port-forward ${POD_GRAFANA} 3000"
     echo -e "kubectl --namespace monitoring port-forward ${POD_PROMETHEUS} 9090\n\n"
 }
 
-
-
 case "$1" in
 create)
-create_cluster
-remove_kube_konfig
-get_cred_for_cluster
-deploy_namespaces
-deploy_rabbit_mongo
-deploy_helm
-deploy_prometheus
-deploy_grafana
-output_values
+    create_cluster
+    remove_kube_konfig
+    get_cred_for_cluster
+    deploy_helm
+    deploy_namespaces
+    deploy_rabbit_mongo
+    deploy_base_app
+    deploy_prometheus
+    deploy_grafana
+    output_values
 ;;
 
 recreate)
-destroy_cluster
-create_cluster
-remove_kube_konfig
-get_cred_for_cluster
-deploy_namespaces
-deploy_rabbit_mongo
-deploy_helm
-deploy_prometheus
-deploy_grafana
-output_values
+    destroy_cluster
+    create_cluster
+    remove_kube_konfig
+    get_cred_for_cluster
+    deploy_helm
+    deploy_namespaces
+    deploy_rabbit_mongo
+    deploy_base_app
+    deploy_prometheus
+    deploy_grafana
+    output_values
 ;;
 
 destroy)
-destroy_cluster
+    destroy_cluster
 ;;
 
 *)
-echo -e "\n${GREEN}create${NONE} - to create new cluster"
-echo -e "${GREEN}recreate${NONE} - to destroy and create cluster"
-echo -e "${GREEN}destroy${NONE} - to destroy existing cluster\n"
+    echo -e "\n${GREEN}create${NONE} - to create new cluster"
+    echo -e "${GREEN}recreate${NONE} - to destroy and create cluster"
+    echo -e "${GREEN}destroy${NONE} - to destroy existing cluster\n"
 ;;
 esac
